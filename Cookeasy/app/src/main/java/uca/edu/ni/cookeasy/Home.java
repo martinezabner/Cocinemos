643c9462b19eb3a63e495d5bc94adb8a5fcbe56c
Franklin <franksprachen@gmail.com>
2020-11-23 00:22:13 -0600

    Logo animation

---
+++ Cookeasy/app/src/main/java/uca/edu/ni/cookeasy/Home.java
@@ -0,0 +1,14 @@
+package uca.edu.ni.cookeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- Cookeasy/app/src/main/java/uca/edu/ni/cookeasy/MainActivity.java
+++ Cookeasy/app/src/main/java/uca/edu/ni/cookeasy/MainActivity.java
@@ -2,13 +2,43 @@ package uca.edu.ni.cookeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    // Variables
+    Animation fadeout;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        // Animations
+        fadeout = AnimationUtils.loadAnimation(this, R.anim.fade_animation);
+
+        // Hooks
+        logo = findViewById(R.id.logo);
+
+        logo.setAnimation(fadeout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
